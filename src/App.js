cbdf976043e4c2427763e2daf1121eebe624aa99
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-05-01 16:04:42 +0530

    Adding new Movies intio the list

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ function App() {
   const [loader, setLoader] = useState(false)
   const [error, setError] = useState(null)
 
+  /* -------------------------------------------------------------------------- */
+  /*                                 FETCH DATA                                 */
+  /* -------------------------------------------------------------------------- */
   const fetchData = useCallback(() => {
     if (movieList.length === 0) { setLoader(true) }
     setError(null)
-
 
     interval = setInterval(async function () {
 
@@ -44,9 +46,35 @@ function App() {
   }, [fetchData])
 
 
+  /* -------------------------------------------------------------------------- */
+  /*                                ADD NEW MOVIE                               */
+  /* -------------------------------------------------------------------------- */
+
+  const [title, setTitle] = useState("")
+  const [openingText, setOpeningText] = useState("")
+  const [releaseDate, setReleaseDate] = useState("")
+
+  const addNewMovieHandeler = (e) => {
+    e.preventDefault()
+    const newMovieObject = { id: Math.random(), title: title, openingText: openingText, releaseDate: releaseDate }
+    console.log(newMovieObject);
+  }
+
 
   return (
     <React.Fragment>
+
+      <section>
+        <form >
+          <input type='text' placeholder='Title' onChange={(e) => { setTitle(e.target.value) }} />
+          <input type='text' placeholder='Opening Text' onChange={(e) => { setOpeningText(e.target.value) }} />
+          <input type='text' placeholder='Release Date' onChange={(e) => { setReleaseDate(e.target.value) }} />
+        </form>
+        <button onClick={addNewMovieHandeler}>Add Movie</button>
+      </section>
+
+
+
       <section>
         <button onClick={fetchData}>Fetch Movies</button>
       </section>
