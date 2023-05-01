4dd45fdbb7a9b1723b3d745258affadd12f6bef7
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-05-01 14:26:19 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -4,23 +4,36 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 
+
+
+let interval;
 function App() {
 
   const [movieList, setMovieList] = useState([])
-
   const [loader, setLoader] = useState(false)
+  const [error, setError] = useState(null)
 
-  const fetchData = async () => {
+  const fetchData = () => {
     if (movieList.length === 0) { setLoader(true) }
+    setError(null)
 
-    const response = await fetch("https://swapi.dev/api/films/")
-    const { results } = await response.json()
-    const newDataArrary = results.map(movie => {
-      return { title: movie.title, releaseDate: movie.release_date, openingText: movie.opening_crawl, id: movie.episode_id }
-    })
 
-    setLoader(false)
-    setMovieList(newDataArrary)
+
+    interval = setInterval(async function () {
+      try {
+        const response = await fetch("https://swapi.dev/api/film/")
+        const { results } = await response.json()
+        const newDataArrary = results.map(movie => {
+          return { title: movie.title, releaseDate: movie.release_date, openingText: movie.opening_crawl, id: movie.episode_id }
+        })
+        setMovieList(newDataArrary)
+        console.log("DATA FETCHED");
+        clearInterval(interval)
+      } catch (error) {
+        setError('Something went wrong ....Retrying')
+      }
+      setLoader(false)
+    }, 1000);
   }
 
 
@@ -32,6 +45,8 @@ function App() {
 
       <section>
         {loader && <h1>Loading</h1>}
+        {error}
+        {error !== null && <button onClick={() => { clearInterval(interval) }}>Stop Retrying</button>}
         <MoviesList movies={movieList} />
       </section>
 
